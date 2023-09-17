2a2d5b8a58c48b78a66643ba978a77876eb671f9
marwanhassan11 <marwanhassan730@gmail.com>
2023-09-17 16:59:23 +0300

    first commit

---
+++ lib/widgets/tune_items.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:tune_app/models/tune_model.dart';
+
+class TuneItem extends StatelessWidget {
+  const TuneItem({
+    Key? key,
+    required this.tune,
+  }) : super(key: key);
+  final TuneModel tune;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          tune.playSound();
+        },
+        child: Container(
+          color: tune.color,
+        ),
+      ),
+    );
+  }
+}
